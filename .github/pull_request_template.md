38a224bb13874655091c5169c24f5a1f43f11aa8
mschabhuettl <github-action@actions-template-sync.noreply.github.com>
2024-10-01 00:15:09 +0000

    chore(template): merge template changes :up:
    Signed-off-by: mschabhuettl <github-action@actions-template-sync.noreply.github.com>

---
+++ .github/pull_request_template.md
@@ -0,0 +1,20 @@
+> [!NOTE]
+I am meticulous when it comes to merging code into the main branch, so please understand that I may reject pull requests that do not meet the project's standards. It's never personal. Also, game-related scripts have a lower chance of being merged.
+
+## Description
+
+Provide a summary of the changes made and/or reference the issue being addressed.
+
+Fixes # (issue)
+
+## Type of change
+
+Please check the relevant option(s):
+
+- [ ] Bug fix (non-breaking change that resolves an issue)
+- [ ] New feature (non-breaking change that adds functionality)
+- [ ] Breaking change (a fix or feature that would cause existing functionality to change unexpectedly)
+- [ ] New script (a fully functional and thoroughly tested script or set of scripts.)
+- [ ] Self-review performed (I have reviewed my code, ensuring it follows established patterns and conventions)
+- [ ] Documentation update required (this change requires an update to the documentation)
+
--- ct/create_lxc.sh
+++ ct/create_lxc.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2024 tteck
-# Copyright (c) 2024 mschabhuettl
-# Author: tteck (tteckster), mschabhuettl
+# Author: tteck (tteckster)
 # License: MIT
-# https://github.com/mschabhuettl/PiBlockLXC/raw/main/LICENSE
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
 
 # This sets verbose mode if the global variable is set to "yes"
 # if [ "$VERBOSE" == "yes" ]; then set -x; fi
@@ -18,7 +17,7 @@ CL=$(echo "\033[m")
 CM="${GN}✓${CL}"
 CROSS="${RD}✗${CL}"
 BFR="\\r\\033[K"
-HOLD="-"
+HOLD=" "
 
 # This sets error handling options and defines the error_handler function to handle errors
 set -Eeuo pipefail
@@ -37,13 +36,13 @@ function error_handler() {
 
 # This function displays a spinner.
 function spinner() {
-  printf "\e[?25l"
-  spinner="/-\\|/-\\|"
-  spin_i=0
-  while true; do
-    printf "\b%s" "${spinner:spin_i++%${#spinner}:1}"
-    sleep 0.1
-  done
+    local chars="/-\|"
+    local spin_i=0
+    printf "\e[?25l"
+    while true; do
+        printf "\r \e[36m%s\e[0m" "${chars:spin_i++%${#chars}:1}"
+        sleep 0.1
+    done
 }
 
 # This function displays an informational message with a yellow color.
@@ -120,7 +119,7 @@ function select_storage() {
   else
     local STORAGE
     while [ -z "${STORAGE:+x}" ]; do
-      STORAGE=$(whiptail --backtitle "PiBlockLXC" --title "Storage Pools" --radiolist \
+      STORAGE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "Storage Pools" --radiolist \
       "Which storage pool you would like to use for the ${CONTENT_LABEL,,}?\nTo make a selection, use the Spacebar.\n" \
       16 $(($MSG_MAX_LENGTH + 23)) 6 \
       "${MENU[@]}" 3>&1 1>&2 2>&3) || exit "Menu aborted."
